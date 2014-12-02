8b0ca76193ad4f99ab9e7c01e5525aefd1b444b4
Pedro Tavares <pedrotavares.dev@gmail.com>
2014-12-02 17:46:38 +0000

    refactor

---
+++ bin.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var moment = require('moment');
+
+var Jekyll = function (options) {
+    this.layout  = 'post',
+    this.title   = '',
+    this.folder  = '',
+    this.path    = '',
+    this.file    = '',
+    this.content = '',
+    this.extension = 'markdown',
+    this.init(options)
+}
+
+Jekyll.prototype.genFilePath = function (isDraft) {
+    this.folder = isDraft ? '_drafts' : '_posts';
+    this.path = path.join(this.folder, this.file);
+}
+
+Jekyll.prototype.genFileName = function (file) {
+    this.title = file;
+    this.date = moment().format('YYYY-MM-DD');
+    this.file = this.date + '-' + this.title + '.' + this.extension;
+}
+
+Jekyll.prototype.genFileContent = function () {
+    this.content = [
+        '---',
+        'layout: ' + this.layout,
+        'title: '  + this.title,
+        this.date,
+        '---'
+    ].join('\n');
+}
+
+Jekyll.prototype.init = function (options) {
+    this.genFileName(options.title);
+    this.genFilePath(options.isDraft);
+    this.genFileContent();
+}
+
+module.exports = Jekyll;
--- index.js
+++ index.js
@@ -1,59 +1,20 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var path = require('path');
-var moment = require('moment');
+var Jekyll = require('./bin');
 
-var Jekyll = function (options) {
-    this.layout  = 'post',
-    this.title   = '',
-    this.folder  = '',
-    this.path    = '',
-    this.file    = '',
-    this.content = '',
-    this.extension = 'markdown',
-    this.init(options)
-}
-
-Jekyll.prototype.genFilePath = function (isDraft) {
-    this.folder = isDraft ? '_drafts' : '_posts';
-    this.path = path.join(this.folder, this.file);
-}
-
-Jekyll.prototype.genFileName = function (file) {
-    this.title = file;
-    this.date = moment().format('YYYY-MM-DD');
-    this.file = this.date + '-' + this.title + '.' + this.extension;
-}
-
-Jekyll.prototype.genFileContent = function () {
-    this.content = [
-        '---',
-        'layout: ' + this.layout,
-        'title: '  + this.title,
-        this.date,
-        '---'
-    ].join('\n');    
-}
-
-Jekyll.prototype.init = function (options) {
-    this.genFileName(options.title);
-    this.genFilePath(options.isDraft);
-    this.genFileContent();
-}
-
-module.exports.new = function (title, options, cb) {    
-    var jkl = new Jekyll({ 
+module.exports.new = function (title, options, cb) {
+    var jkl = new Jekyll({
         title: title,
-        isDraft: options.isDraft 
+        isDraft: options.isDraft
     });
- 
+
     !fs.existsSync(jkl.folder) && fs.mkdirSync(jkl.folder);
-    
+
     if (fs.existsSync(jkl.path)) {
         return cb('file exists');
     }
-    
+
     fs.writeFile(jkl.path, jkl.content, 'utf8', function(err) {
         if (err) {
             cb(err);
@@ -61,4 +22,3 @@ module.exports.new = function (title, options, cb) {
         cb(null, jkl.path);
     });
 }
-
